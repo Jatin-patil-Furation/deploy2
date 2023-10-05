e5e5496b3d05bd7904b7dd440168cf3c0cacbbf0
Jatin Patil <137898138+Jatin-patil-Furation@users.noreply.github.com>
2023-10-05 18:44:25 +0530

    Update page.jsx

--- app/teen-patti/page.jsx
+++ app/teen-patti/page.jsx
@@ -101,7 +101,7 @@ const LandscapePage = () => {
     setLoggeduser(() => Loggeduser);
     console.log(privateTableKey);
     console.log("connection start ");
-    socketRef.current = io("http://localhost:8000/", {
+    socketRef.current = io("https://newsocket.onrender.com/", {
       transports: ["websocket"],
     });
     socketRef.current.on("connect", () => {
